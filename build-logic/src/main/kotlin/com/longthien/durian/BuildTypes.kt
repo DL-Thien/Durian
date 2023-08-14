6739c32e926d2912b7da29d0eb820438d4545f97
DL-THIEN\longt <longthien.dinh1201@gmail.com>
2023-08-14 20:41:00 +0700

    setup gradle build version catalog convention plugin

---
+++ build-logic/src/main/kotlin/com/longthien/durian/BuildTypes.kt
@@ -0,0 +1,23 @@
+package com.longthien.durian
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+
+internal fun Project.configureBuildType(commonExtension: CommonExtension<*, *, *, *>) {
+    commonExtension.apply {
+        buildTypes {
+
+            getByName("debug") {
+                isMinifyEnabled = false
+            }
+
+            getByName("release") {
+                isMinifyEnabled = true
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+    }
+}
---
+++ build-logic/src/main/kotlin/com/longthien/durian/DurianAppConfig.kt
@@ -0,0 +1,6 @@
+package com.longthien.durian
+
+object DurianAppConfig {
+    const val minSdk = 26
+    const val compileSdk = 33
+}
